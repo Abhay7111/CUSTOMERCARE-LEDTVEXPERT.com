b37c0e80bf00927dc554e7f401a1571151c50d76
Abhay vishwakarma <142080096+Abhay7111@users.noreply.github.com>
2024-07-04 05:03:56 +0530

    nav updated

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,10 +4,11 @@ import Footer from './Footer'
 
 function Home() {
   return (
-    <div>
-     <Nav/>
-          Home
-     <Footer/>
+    <div className='w-full relative z-10 h-[100vh] bg-green-400'>
+          <div className='w-full h-full bg-red-400 absolute z-0 overflow-hidden'>
+               <div className='size-40 relative rounded-full blur-3xl top-botom-animation'></div>
+          </div>
+          <div className='relative z-10'>fd</div>
      </div>
   )
 }
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function Nav() {
+     useEffect(()=>{
+          const menu1 = document.querySelector(".menu1");
+          const menu = document.querySelector(".menu");
+
+          menu1.onclick = function(){
+               if(menu.style.display =='block'){
+                    menu.style.display = 'none';
+               }else{
+                    menu.style.display = 'block';
+               }
+          }
+     })
   return (
-    <div className='w-full h-14 flex items-center justify-between px-3 bg-red-400'>
+    <div className='w-full h-14 flex items-center justify-between px-3'>
      <div className='text-xl font-bold'>CL.</div>
-     <div className='w-fit bg-green-400/20'>
-          <ul className='flex flex-col sm:flex-row gap-2'>
-               <a href="#"><li className='text-sm font-medium px-3 py-1 rounded-md transition-all hover:bg-yellow-400'>Home</li></a>
-               <a href="#"><li className='text-sm font-medium px-3 py-1 rounded-md transition-all hover:bg-yellow-400'>About</li></a>
-               <a href="#"><li className='text-sm font-medium px-3 py-1 rounded-md transition-all hover:bg-yellow-400'>Our service</li></a>
-               <a href="#"><li className='text-sm font-medium px-3 py-1 rounded-md transition-all hover:bg-yellow-400'>Contact us</li></a>
-               <a href="#"><li className='text-sm font-medium px-3 py-1 rounded-md transition-all hover:bg-yellow-400'>Gallery</li></a>
+     <div className='w-fit relative'>
+          <ul className='hidden menu absolute bg-zinc-100 py-2 sm:py-0 mt-3 sm:mt-0 sm:bg-transparent top-full w-fit text-nowrap px-4 sm:px-0 rounded-md sm:rounded-none right-0 sm:relative sm:flex sm:flex-row gap-2'>
+               <a href="#"><li className='text-md sm:text-sm font-medium px-3 py-1 rounded-md transition-all hover:bg-yellow-400'>Home</li></a>
+               <a href="#"><li className='text-md sm:text-sm font-medium px-3 py-1 rounded-md transition-all hover:bg-yellow-400'>About</li></a>
+               <a href="#"><li className='text-md sm:text-sm font-medium px-3 py-1 rounded-md transition-all hover:bg-yellow-400'>Gallery</li></a>
+               <a href="#"><li className='text-md sm:text-sm font-medium px-3 py-1 rounded-md transition-all hover:bg-yellow-400'>Our service</li></a>
+               <a href="#"><li className='text-md sm:text-sm font-medium px-3 py-1 rounded-md transition-all hover:bg-yellow-400'>Contact us</li></a>
           </ul>
+          <span className='text-2xl font-semibold sm:hidden cursor-pointer'><i class="ri-menu-line menu1"></i></span>
      </div>
      </div>
   )
