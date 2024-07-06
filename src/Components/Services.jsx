636bdbad7da7ed2a9686da833fa584a60df872bf
Abhay vishwakarma <142080096+Abhay7111@users.noreply.github.com>
2024-07-06 13:34:10 +0530

    fd

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import Backligh from '/BackLight.jpg'
 
 function Services() {
   return (
     <div className='flex relative flex-col sm:flex-row items-center sm:justify-center flex-wrap'>
      <h1 className='w-full text-center text-[6vw] sm:text-[4vw] font-bold uppercase'>Our services</h1>
      <div className='w-full h-fit flex items-center justify-center flex-wrap mt-3'>
-      <div className='w-72 min-h-20 bg-yellow-500 rounded-lg px-1'>
-        <img src="" alt="Not found" className='text-sm font-thin' />
+      <div className='w-72 min-h-20 bg-yellow-500 rounded-lg p-1.5'>
+        <img src={Backligh} alt="Not found" className='text-sm font-thin rounded-md' />
       </div>
      </div>
     </div>
